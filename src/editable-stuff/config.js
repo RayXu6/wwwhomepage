38d8a34574c4aac2f0b92ca8103bca5694e1cda7
RayXu6 <3521838694@qq.com>
2023-04-15 20:35:26 +0800

    Initial commit

---
+++ src/editable-stuff/config.js
@@ -0,0 +1,139 @@
+// Navigation Bar SECTION
+import { ICON_NAME } from 'react-icons/md'
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "RayXu6",
+  middleName: "",
+  lastName: "",
+  message: " Confidence is 10% work and 90% delusion. ",
+  icons: [
+    {
+      image: "",
+      url: "https://github.com/rayxu6",
+    },
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+
+const about = {
+  show: true,
+  heading: "关于我",
+  message:
+    "我是RayXu6，只是一名高中生，什么都会去玩一玩，有时会写写代码，随缘发视频，喜欢Minecraft，其中最喜欢速通，会一点开服，只有周末会瞎玩玩，平时认真学习。",
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "最近的项目",
+  gitHubUsername: "rayxu6", //i.e."johnDoe12Gh"
+  reposLength: 4,
+  specificRepos: [],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    {
+      img: require("../editable-stuff/hashirshoaeb.png"),
+      label: "First slide label",
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+      img: require("../editable-stuff/hashirshoaeb.png"),
+      label: "Second slide label",
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+  ],
+  imageSize: {
+    width:"615",
+    height:"450"
+  }
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "技能",
+  hardSkills: [
+    { name: "Python", value: 90 },
+    { name: "SQL", value: 75 },
+    { name: "RUST", value: 50 },
+    { name: "JAVA", value: 65 },
+    { name: "C/C++", value: 65 },
+    { name: "HTML/CSS", value: 90 },
+    { name: "JavaScript", value: 90 },
+    { name: "React", value: 65 },
+    { name: "VUE", value: 60 },,
+    { name: "PHP", value: 60 },
+  ],
+  softSkills: [
+    { name: "Goal-Oriented", value: 80 },
+    { name: "Collaboration", value: 90 },
+    { name: "Positivity", value: 75 },
+    { name: "Adaptability", value: 85 },
+    { name: "Problem Solving", value: 75 },
+    { name: "Empathy", value: 90 },
+    { name: "Organization", value: 70 },
+    { name: "Creativity", value: 90 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "联系方式",
+  message:
+    "如果你有任何意见，欢迎提出来，我的邮箱",
+  email: "rayxu6@qq.com",
+};
+
+const experiences = {
+  show: false,
+  heading: "Experiences",
+  data: [
+    {
+      role: 'Software Engineer',// Here Add Company Name
+      companylogo: require('../assets/img/dell.png'),
+      date: 'June 2018 – Present',
+    },
+    {
+      role: 'Front-End Developer',
+      companylogo: require('../assets/img/boeing.png'),
+      date: 'May 2017 – May 2018',
+    },
+  ]
+}
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
